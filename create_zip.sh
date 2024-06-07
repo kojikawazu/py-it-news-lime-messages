ea6efb8de40827a3f6184d4e83a5978d3379aff2
kojikawazu <nanairo7.daydream21@gmail.com>
2024-06-08 01:14:24 +0900

    bash調整

--- create_zip.sh
+++ create_zip.sh
@@ -1,14 +1,45 @@
 #!/bin/bash
+# ----------------------------------------------------------
+# [Name]
+# Lambdaアップロードするzipファイルの生成
+#
+# [Arguments]
+#  - None
+#
+# [Return]
+#  - 0(成功)
+#
+# [Since]
+# 2024/06/08
+# ----------------------------------------------------------
 
-main(){
-  rm -rf package
-  mkdir package
-  pip install -r requirements.txt -t package/
-  cp news_to_line.py package/
-  cp .env package/
-  cd package
-  zip -r ../news_to_line.zip .
+readonly RESULT_SUCCESSED=0
+readonly RESULT_FAILED=1
+
+create_zip() {
+  local -r _PACKAGE_NAME=package
+  local -r _REQUIREMENT_TXT_FILE_NAME=requirements.txt
+  local -r _APP_FILE_NAME=news_to_line.py
+  local -r _ZIP_FILE_NAME=news_to_line.zip
+  
+  rm -rf ${_PACKAGE_NAME}
+  mkdir ${_PACKAGE_NAME}
+
+  pip install -r ${_REQUIREMENT_TXT_FILE_NAME} -t ${_PACKAGE_NAME}/
+  
+  cp ${_APP_FILE_NAME} ${_PACKAGE_NAME}/
+  cp .env ${_PACKAGE_NAME}/
+  cd ${_PACKAGE_NAME}
+  
+  zip -r ../${_ZIP_FILE_NAME} .
   cd ..
+
+  return ${RESULT_SUCCESSED}
+}
+
+main() {
+  create_zip
+  return ${RESULT_SUCCESSED}
 }
 
 main "$@"
